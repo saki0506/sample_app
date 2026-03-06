3cc40aeaaa52164dab07c9589949657077107d43
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-06 21:29:28 +0900

    メールアドレスの小文字化とインデックスの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_05_092303) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_06_120512) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
