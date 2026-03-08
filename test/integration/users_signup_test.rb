a6185d05f49592c4b563f749b0bdc1a9855877e4
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-09 02:01:32 +0900

    エラーメッセージのテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_response :unprocessable_entity
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
   end
 end
