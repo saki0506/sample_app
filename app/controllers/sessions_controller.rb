b8dc781a540a7863daf83b9c57696b8f65c376dc
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-10 21:23:38 +0900

    ユーザーの検索と認証を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
   end
-end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功の処理（後で書く）
+    else
+      render 'new', status: :unprocessable_entity
+    end       # ← if文のend
+  end         # ← createアクションのend
+
+  def destroy
+  end
+
+end           # ← classのend
