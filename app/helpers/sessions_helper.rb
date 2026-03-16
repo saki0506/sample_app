5f4f02c759f10b3324cf4ce54dd3f137213bcc7f
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-16 23:49:54 +0900

    current_userメソッドを修正する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す
+  # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
 end
