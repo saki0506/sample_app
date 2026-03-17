fe01bd8bc0ee32795e7938459b59e8bee8224e66
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-18 03:13:18 +0900

    テストを追加してセーフナビゲーション演算子を使う

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -28,4 +28,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
   end
 
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email,
+                                          password: "invalid" } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
 end
