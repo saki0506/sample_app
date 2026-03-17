c8e312fee52d29db15a652cac1ac47146f3fc3fa
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-18 03:55:54 +0900

    ログアウト機能を追加する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,10 +13,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
-
   end
 
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,4 +17,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 現在のユーザーをログアウトする
+  def log_out
+    reset_session
+    @current_user = nil # 安全のため
+  end
+
 end
