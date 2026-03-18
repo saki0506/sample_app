3e741c73093ee71e494bacc509ed4e00cd4b7f43
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-19 05:25:22 +0900

    Remember meのテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -72,3 +72,17 @@ class LogoutTest < Logout
     assert_redirected_to root_url
   end
 end
+
+# ↓ここに追加！LogoutTestの外側！
+class RememberingTest < UsersLogin
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
