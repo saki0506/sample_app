e169fd533ba2f09464370b8ba59678bfe1feb478
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-19 19:59:46 +0900

    assignsを使ったRemember meテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -73,16 +73,16 @@ class LogoutTest < Logout
   end
 end
 
-# ↓ここに追加！LogoutTestの外側！
 class RememberingTest < UsersLogin
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
   test "login without remembering" do
     log_in_as(@user, remember_me: '1')
+    delete logout_path
     log_in_as(@user, remember_me: '0')
-    assert cookies[:remember_token].blank?
+    assert_empty cookies[:remember_token]
   end
 end
