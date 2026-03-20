11f991e17f78a24b1feee9612f52d1e7b26994da
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-21 06:01:31 +0900

    管理者属性のテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user       = users(:michael)
-    @other_user = users(:archer)  # ← 追加！
+    @other_user = users(:archer)
   end
 
   test "should get new" do
@@ -25,14 +25,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "should redirect edit when logged in as wrong user" do  # ← 追加！
+  test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in as wrong user" do  # ← 追加！
+  test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
@@ -41,7 +41,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect index when not logged in" do
-  get users_path
-  assert_redirected_to login_url
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: "password",
+            password_confirmation: "password",
+            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
-   end
