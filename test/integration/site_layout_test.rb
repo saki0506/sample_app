864d66b43ecf71b946efd4a4a131cb66e5c8ea7f
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-21 03:49:51 +0900

    レイアウトリンクのテストを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_select "a[href=?]", root_path, count: 2
@@ -9,5 +14,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links when logged in" do        # ← 追加！
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
