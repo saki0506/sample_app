1528943d1add87fc12ea7d6fd97e8cd194040699
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-22 17:28:36 +0900

    アカウント有効化のテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -76,13 +76,14 @@ end
 class RememberingTest < UsersLogin
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    # assert_equal cookies[:remember_token], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
+    # Cookie を保存してログイン
     log_in_as(@user, remember_me: '1')
-    delete logout_path
+    # Cookie が削除されていることを検証してからログイン
     log_in_as(@user, remember_me: '0')
-    assert_empty cookies[:remember_token]
+    assert cookies[:remember_token].blank?
   end
 end
