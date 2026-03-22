6465c59ccbd099ec1ee4e43964c71023310680aa
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-03-22 23:04:19 +0900

    Mailgun API方式に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,18 +74,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :mailgun
   host = 'sample-app-x49f.onrender.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port              => 465,
-    :address           => 'smtp.mailgun.org',
-    :enable_starttls_auto => false,
-    :tls => true,
-    :user_name         => ENV['MAILGUN_SMTP_LOGIN'],
-    :password          => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain            => host,
-    :authentication    => :plain,
+  ActionMailer::Base.mailgun_settings = {
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain:  'sandbox3e02fa9269cc45b1b287f496e176bde9.mailgun.org',
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
